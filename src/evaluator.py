1f37511f68a92ed05b16187ff6d5c9ca924495f7
tonyjo <tjoseph.ubc@gmail.com>
2021-03-21 16:16:31 -0700

    first working version

--- src/evaluator.py
+++ src/evaluator.py
@@ -12,149 +12,142 @@ from primitives import _totensor
 class Env(dict):
     "An environment: a dict of {'var': val} pairs, with an outer Env."
     def __init__(self, parms=(), args=(), outer=None):
-        self.update(zip(parms, args))
-        self.outer = {**outer}
+        self.new_env = pmap({p:a for p, a in zip(parms, args)})
+        self.new_env = self.new_env.update({'alpha':''})
+        self.outer   = outer
+    def does_exist(self, var):
+        if var in self.new_env:
+            return True
+        elif self.outer is None:
+            return False
+        else:
+            return self.outer.does_exist(var=var)
     def find(self, var):
         "Find the innermost Env where var appears."
-        if (var in self):
-            return self
+        if (var in self.new_env):
+            return self.new_env[var]
         else:
             return self.outer.find(var)
+        # return self.new_env[var] if (var in self.new_env) else self.outer.find(var)
 
-class Procedure(object):
-    "A user-defined Scheme procedure."
-    def __init__(self, parms, body, env, sig):
-        self.parms, self.body, self.env, self.sig = parms, body, env, sig
-    def __call__(self, *args):
-        env = Env(parms=self.parms, args=args, outer=self.env)
-        print('----->', env)
-        print('----->', env.outer)
-        print('----->', dir(env))
-        return evaluate(exp=self.body, sig=self.sig, env=env)
 
 def standard_env():
     "An environment with some Scheme standard procedures."
-    # env = pmap(penv)
-    # env = env.update({'alpha' : []})
-    # return env
+    env = Env(parms=penv.keys(), args=penv.values())
 
-    penv.update({'alpha' : []})
-    return penv
+    return env
 
-global_env = standard_env()
 
-DEBUG=True
-def evaluate(exp, sig, env):
-    if env is None:
-        env = standard_env()
+class Procedure(object):
+    "A user-defined Scheme procedure."
+    def __init__(self, parms, body, env):
+        self.parms = parms
+        self.body  = body
+        self.env   = env
+    def __call__(self, *args, sig):
+        return eval(exp=self.body, sig=sig, env=Env(parms=self.parms, args=args, outer=self.env))
+
+
+DEBUG=False
+def eval(exp, sig, env):
     #import pdb; pdb.set_trace()
     if DEBUG:
         print('Exp: ', exp)
-        print('Env: ', env, '\n')
-        try:
-            print('====>', env.outer)
-            print('====>', dir(env))
-        except:
-            print('====>', dir(env))
-        print('\n')
 
     # variable reference
-    if isinstance(exp, str):
-        import pdb; pdb.set_trace()
-        if DEBUG:
-            print('Exp: ', exp)
-            print('Env: ', env.keys(), '\n')
-            try:
-                print('====>', env.outer)
-                print('====>', dir(env))
-            except:
-                print('====>', dir(env))
-            print('\n')
+    if isinstance(exp, str): #and (env.does_exist(var=exp)):
+        #import pdb; pdb.set_trace()
         try:
             env_func = env.find(exp)
-            #import pdb; pdb.set_trace()
             if DEBUG:
                 print('env_func: ', env_func)
-            return [env_func[exp], sig]
+            return env_func, sig
         except:
-            try:
-                env_func = env[exp]
-                if DEBUG:
-                    print('env_func: ', env_func)
-                return [env_func, sig]
-            except:
-                return[exp, sig]
+            return exp, sig
 
     # constant
-    if not isinstance(exp, list):
+    if not isinstance(exp, list): #and (not env.does_exist(var=exp)):
         if isinstance(exp, int) or isinstance(exp, float):
             exp = _totensor(exp)
-        return [exp, sig]
-
-    op, addr, *args = exp
-    if DEBUG:
-        print('OP: ', op)
-        print('addr: ', addr)
-        print('args: ', args)
-        print('Env', env, '\n')
-
-    # address
-    if addr[0] == 'push-address':
-        addr_op, *addr_args = addr
-        # import pdb; pdb.set_trace()
-        if DEBUG:
-            print('addr_op: ', addr_op)
-            print('addr_args: ', addr_args)
-            print('Env: ', env)
-        proc, _ = evaluate(exp=addr_op, sig=sig, env=env)
-        if DEBUG:
-            print('proc: ', proc)
-        alpha_key, addr_ = addr_args
-        if DEBUG:
-            print('Key: ', alpha_key)
-            print('Key params: ', addr_)
-        alpha, _  = evaluate(exp=alpha_key, sig=sig, env=env)
-        if DEBUG:
-            print('alpha: ', alpha)
-        try:
-            eval_addr = proc(alpha, [addr_])
-        except:
-            eval_addr = [alpha] + [addr_]
-        if DEBUG:
-            print('eval_addr: ', eval_addr, '\n')
-        # update env
-        env.update({alpha_key: eval_addr})
+        return exp, sig
 
     # definition -- functions as first class objects
-    if op == 'fn':
-        parms = addr[1:]
-        if isinstance(args, list) and len(args) == 1:
-            try:
-                if len(args[0]) > 1:
-                    args = args[0]
-            except:
-                # most likely int object
-                args = args[0]
+    if exp[0] == 'fn':
+        #import pdb; pdb.set_trace()
+        parms, body = exp[1], exp[2]
         if DEBUG:
-            print('Parm: ', parms)
-            print('Body: ',  args, '\n')
+            print('Parm: ',  parms)
+            print('Body: ',  body, '\n')
+        return Procedure(parms=parms, body=body, env=env), sig
 
-        return [Procedure(parms, args, env, sig), sig]
+    # conditional
+    if exp[0] == 'if':
+        if len(exp) == 4:
+            bcond, consq, alter = exp[1], exp[2], exp[3]
+            bcond_eval, sig = eval(exp=bcond, sig=sig, env=env)
+            if bcond_eval:
+                return eval(exp=consq, sig=sig, env=env)
+            else:
+                return eval(exp=alter, sig=sig, env=env)
+        else:
+            bcond, addr, op1, op2 = exp[0][0], exp[0][1], exp[0][2], exp[0][3]
+            bcond_eval, sig = eval(exp=bcond, sig=sig, env=env)
+            op1_eval, sig   = eval(exp=op1, sig=sig, env=env)
+            op2_eval, sig   = eval(exp=op2, sig=sig, env=env)
+            proc_eval, sig  = bcond_eval(op1_eval, op2_eval)
+            return proc_eval, sig
+
+    # observe
+    elif exp[0] == "observe":
+        addr, dist, obs = exp[1], exp[2], exp[3]
+        addr_eval, sig  = eval(exp=addr, sig=sig, env=env)
+        dist_eval, sig  = eval(exp=dist, sig=sig, env=env)
+        obs_eval,  sig  = eval(exp=obs,  sig=sig, env=env)
+        return obs_eval, sig
+
+    # sample
+    elif exp[0] == "sample":
+        addr, dist = exp[1], exp[2]
+        addr_eval, sig = eval(exp=addr, sig=sig, env=env)
+        dist_eval, sig = eval(exp=dist, sig=sig, env=env)
+        try:
+            dist_eval = dist_eval.sample()
+        except Exception as e:
+            print(dist_eval)
+            print('Failed to sample!')
+        return dist_eval, sig
 
     # procedure call
     else:
-        proc, sig = evaluate(exp=op, sig=sig, env=env)
+        # Evaluate procedure
+        proc, sig = eval(exp=exp[0], sig=sig, env=env)
+        # Evaluate arguments
         vals = []
-        for arg in args:
-            try:
-                arg_eval, sig = evaluate(arg, sig, env)[0]
-            except:
-                arg_eval, sig = evaluate(arg, sig, env)
+        for each_exp in range(1, len(exp)):
+            arg_eval, sig = eval(exp=exp[each_exp], sig=sig, env=env)
             vals.append(arg_eval)
         if DEBUG:
+            print('exp : ', exp)
             print('proc: ', proc)
             print('vals: ', vals, '\n')
-        return [proc(*vals), sig]
+        try:
+            if type(proc) == Procedure:
+                proc_eval, sig = proc(*vals, sig=sig)
+            else:
+                proc_eval = proc(*vals)
+            return proc_eval, sig
+        except Exception as e:
+            print('Exception raised: ', e)
+            print('proc: ', proc)
+            print('vals: ', vals, '\n')
+            # Tensor object
+            return proc, sig
+#-----------------------------------------------------------------------
+def evaluate(exp):
+    global_env = standard_env()
+    proc, sig = eval(exp=exp, sig={}, env=global_env)
+
+    return proc(sig=sig)
 
 
 def get_stream(exp):
@@ -163,7 +156,6 @@ def get_stream(exp):
 
 
 def run_deterministic_tests():
-
     # for i in range(1,14):
     # #for i in range(13,14):
     #     # # Note: this path should be with respect to the daphne path!
@@ -173,79 +165,75 @@ def run_deterministic_tests():
     #     #     json.dump(ast, fout, indent=2)
     #     # print('\n\n\nSample of posterior of program {}:'.format(i))
     #
-    #     print('\nSample of posterior of program {}:'.format(i))
+    #     print('\nDeterministic program {}:'.format(i))
     #     ast_path = f'./jsons/tests/deterministic/{i}.json'
     #     with open(ast_path) as json_file:
     #         exp = json.load(json_file)
     #     # print(exp)
     #
-    #     exp = exp[2:][0]
-    #     ret, sig = evaluate(exp, sig={}, env=global_env)
-    #
+    #     ret, sig = evaluate(exp=exp)
     #     truth = load_truth(f'{program_path}/src/programs/tests/deterministic/test_{i}.truth')
+    #     print('Output: ', ret)
+    #     print('Truth:', truth)
     #     try:
     #         assert(is_tol(ret, truth))
+    #         print('HOPPL Test passed!')
     #     except:
-    #         raise AssertionError('return value {} is not equal to truth {} for exp {}'.format(ret,truth,exp))
-    #     print('FOPPL Tests passed')
-    #
-    #     global_env.update({'alpha' : []})
+    #         print('return value {} is not equal to truth {} for exp {}'.format(ret,truth,exp))
 
-    for i in range(5,6):
-    #for i in range(1,13):
-        # # Note: this path should be with respect to the daphne path!
-        # ast = daphne(['desugar-hoppl', '-i', f'{program_path}/src/programs/tests/hoppl-deterministic/test_{i}.daphne'])
-        # ast_path = f'./jsons/tests/hoppl-deterministic/{i}.json'
-        # with open(ast_path, 'w') as fout:
-        #     json.dump(ast, fout, indent=2)
-        # print('\n\n\nSample of posterior of program {}:'.format(i))
+    #for i in range(5,6):
+    for i in range(1,13):
+    #     # Note: this path should be with respect to the daphne path!
+    #     ast = daphne(['desugar-hoppl', '-i', f'{program_path}/src/programs/tests/hoppl-deterministic/test_{i}.daphne'])
+    #     ast_path = f'./jsons/tests/hoppl-deterministic/{i}.json'
+    #     with open(ast_path, 'w') as fout:
+    #         json.dump(ast, fout, indent=2)
+    #     print('\n\n\nSample of posterior of program {}:'.format(i))
 
+        print('\nHOPPL deterministic  {}:'.format(i))
         ast_path = f'./jsons/tests/hoppl-deterministic/{i}.json'
         with open(ast_path) as json_file:
             exp = json.load(json_file)
-        # print(graph)
+        #print(exp)
 
-        exp = exp[2:]
-        if len(exp) == 1:
-            exp = exp[0]
-        ret, sig = evaluate(exp=exp, sig={}, env=global_env)
-        #print(ret)
-
-    #     # try:
-    #     #     assert(is_tol(ret, truth))
-    #     # except:
-    #     #     raise AssertionError('return value {} is not equal to truth {} for exp {}'.format(ret,truth,exp))
-    #
-    #     print('Test passed')
-
-    print('All deterministic tests passed')
-
+        ret, sig = evaluate(exp=exp)
+        truth = load_truth(f'{program_path}/src/programs/tests/hoppl-deterministic/test_{i}.truth')
+        try:
+            print('Output: ', ret)
+            print('Truth:', truth)
+            assert(is_tol(ret, truth))
+            print('Test passed')
+        except:
+            print('return value is not equal to truth {}'.format(truth))
+    print('All hoppl-deterministic tests passed')
 
 
 def run_probabilistic_tests():
-
     num_samples=1e4
     max_p_value = 1e-2
 
-    for i in range(1,7):
+    #for i in range(1,7):
+    for i in range(4,5):
         # # Note: this path should be with respect to the daphne path!
         # ast = daphne(['desugar-hoppl', '-i', f'{program_path}/src/programs/tests/probabilistic/test_{i}.daphne'])
         # ast_path = f'./jsons/tests/probabilistic/{i}.json'
         # with open(ast_path, 'w') as fout:
         #     json.dump(ast, fout, indent=2)
-        # print('\n\n\nSample of posterior of program {}:'.format(i))
+        # print('\n\n\nSample of prior of program {}:'.format(i))
 
-        ast_path = f'./jsons/tests/deterministic/{i}.json'
+        ast_path = f'./jsons/tests/probabilistic/{i}.json'
         with open(ast_path) as json_file:
-            graph = json.load(json_file)
-        # print(graph)
+            ast = json.load(json_file)
+        # print(ast)
 
-        # stream = get_stream(exp)
-        #
-        # p_val = run_prob_test(stream, truth, num_samples)
-        #
-        # print('p value', p_val)
-        # assert(p_val > max_p_value)
+        #ret, sig = evaluate(exp=ast)
+        #print(ret)
+
+        stream = get_stream(exp=ast)
+        truth  = load_truth(f'{program_path}/src/programs/tests/probabilistic/test_{i}.truth')
+        p_val  = run_prob_test(stream, truth, num_samples)
+        print('p value', p_val)
+        assert(p_val > max_p_value)
 
     print('All probabilistic tests passed')
 
@@ -254,12 +242,21 @@ def run_probabilistic_tests():
 if __name__ == '__main__':
     program_path = '/home/tonyjo/Documents/prob-prog/CS539-HW-5'
 
-    run_deterministic_tests()
-    # run_probabilistic_tests()
+    #run_deterministic_tests()
+    #run_probabilistic_tests()
 
+    #for i in range(3,4):
+    for i in range(1,4):
+        # # Note: this path should be with respect to the daphne path!
+        # ast = daphne(['desugar-hoppl', '-i', f'{program_path}/src/programs/{i}.daphne'])
+        # ast_path = f'./jsons/{i}.json'
+        # with open(ast_path, 'w') as fout:
+        #     json.dump(ast, fout, indent=2)
+        # print('\n\n\nSample of prior of program {}:'.format(i))
 
-    # for i in range(1,4):
-    #     print(i)
-    #     exp = daphne(['desugar-hoppl', '-i', '../../HW5/programs/{}.daphne'.format(i)])
-    #     print('\n\n\nSample of prior of program {}:'.format(i))
-    #     print(evaluate(exp))
+        ast_path = f'./jsons/{i}.json'
+        with open(ast_path) as json_file:
+            ast = json.load(json_file)
+
+        ret, sig = evaluate(exp=ast)
+        print(ret)
--- src/primitives.py
+++ src/primitives.py
@@ -1,10 +1,11 @@
 import torch
+from copy import deepcopy
 import torch.distributions as dist
 
 #--------------------------Useful functions and OPS ---------------------------#
 class Normal(dist.Normal):
 
-    def __init__(self, alpha, loc, scale):
+    def __init__(self, loc, scale):
 
         if scale > 20.:
             self.optim_scale = scale.clone().detach().requires_grad_()
@@ -33,8 +34,8 @@ class Normal(dist.Normal):
 
         return super().log_prob(x)
 
-
-def _hashmap(x):
+def _hashmap(*x):
+    x = x[1:]
     # List (key, value, key, value, ....)
     return_x = {}
     if len(x)%2 == 0:
@@ -44,45 +45,57 @@ def _hashmap(x):
             else:
                 key_value  = x[i]
             value = x[i+1]
-            if not torch.is_tensor(value):
-                if isinstance(value, list):
-                    value = torch.tensor(value, dtype=x.dtype)
-                else:
-                    value = torch.tensor([value], dtype=x.dtype)
-            return_x[key_value] = value
+            try:
+                if not torch.is_tensor(value):
+                    if isinstance(value, list):
+                        value = torch.tensor(value, dtype=x.dtype)
+                    else:
+                        value = torch.tensor([value], dtype=x.dtype)
+                return_x[key_value] = value
+            except:
+                return_x[key_value] = value
     else:
         raise IndexError('Un-even key-value pairs')
 
     return return_x
 
+def _totensor(*x):
+    dtype=torch.float32
+    if not torch.is_tensor(x):
+        if isinstance(x, list):
+            x = torch.tensor(x, dtype=dtype)
+        else:
+            x = torch.tensor([x], dtype=dtype)
+    return x
 
-def _vector(x):
+def _vector(*x):
+    x = x[1:]
     if isinstance(x, list):
         return torch.tensor(x, dtype=torch.float32)
     else:
         # Maybe single value
         return torch.tensor([x], dtype=torch.float32)
 
-
-def _put(x, idx_or_key, value):
-    if isinstance(x, dict):
+def _put(addr, x, idx_or_key, value):
+    x_= deepcopy(x)
+    if isinstance(x_, dict):
         if torch.is_tensor(idx_or_key):
             idx_or_key  = idx_or_key.item()
         try:
             if not torch.is_tensor(value):
                 value = _vector(x=x)
-            x[idx_or_key] = value
+            x_[idx_or_key] = value
         except:
             raise IndexError('Key {} cannot put in the dict'.format(idx_or_key))
-        return x
-    elif isinstance(x, list):
+        return x_
+    elif isinstance(x_, list):
         try:
-            x[idx_or_key] = value
+            x_[idx_or_key] = value
         except:
             raise IndexError('Index {} is not present in the list'.format(idx_or_key))
 
-        return x
-    elif torch.is_tensor(x):
+        return x_
+    elif torch.is_tensor(x_):
         try:
             try:
                 if idx_or_key.type() == 'torch.FloatTensor':
@@ -91,22 +104,21 @@ def _put(x, idx_or_key, value):
                 idx_or_key = torch.tensor(idx_or_key, dtype=torch.long)
             if not torch.is_tensor(value):
                 value = torch.tensor(value, dtype=x.dtype)
-            if len(x.size()) > 1:
+            if len(x_.size()) > 1:
                 try:
-                    x[0, idx_or_key] = value
+                    x_[0, idx_or_key] = value
                 except:
-                    x[idx_or_key] = value
+                    x_[idx_or_key] = value
             else:
-                x[idx_or_key] = value
+                x_[idx_or_key] = value
         except:
             raise IndexError('Index {} is not present in the list'.format(idx_or_key))
 
-        return x
+        return x_
     else:
          raise AssertionError('Unsupported data structure')
 
-
-def _remove(x, idx_or_key):
+def _remove(addr, x, idx_or_key):
     if isinstance(x, dict):
         try:
             if isinstance(idx_or_key, float):
@@ -133,8 +145,7 @@ def _remove(x, idx_or_key):
     else:
          raise AssertionError('Unsupported data structure')
 
-
-def _append(x, value):
+def _append(addr, x, value):
     if isinstance(x, list):
         if isinstance(value, list):
             x.extend(value)
@@ -147,18 +158,23 @@ def _append(x, value):
                 value = torch.tensor(value, dtype=x.dtype)
             else:
                 value = torch.tensor([value], dtype=x.dtype)
+        if x.dtype != value.dtype:
+            value = value.type(x.dtype)
         try:
-            if len(x.size()) > 1:
+            if len(x.size()) > 1 and (not(x.shape[0] == 1 and x.shape[1] == 1)):
                 x = torch.squeeze(x)
+            elif len(x.size()) > 1 and (x.shape[0] == 1 and x.shape[1] == 1):
+                x = torch.squeeze(x, dim=1)
             x = torch.cat((x, value))
-        except:
-            raise AssertionError('Cannot append the torch tensors')
+        except Exception as e:
+            print(x.dtype)
+            print(value.dtype)
+            raise AssertionError('Cannot append the torch tensors, due to: ', str(e))
         return x
     else:
         raise AssertionError('Unsupported data structure')
 
-
-def _get(x, idx):
+def _get(addr, x, idx):
     if isinstance(x, dict):
         if torch.is_tensor(idx):
             idx = idx.item()
@@ -181,74 +197,171 @@ def _get(x, idx):
     else:
         raise AssertionError('Unsupported data structure')
 
-
-def _squareroot(x):
+def _squareroot(addr, x):
     if not torch.is_tensor(x):
         x = torch.tensor(x, dtype=torch.float32)
     return torch.sqrt(x)
 
+def _add(addr, a, b):
+    return torch.add(a, b)
 
-def _totensor(x, dtype=torch.float32):
-    if not torch.is_tensor(x):
-        if isinstance(x, list):
-            x = torch.tensor(x, dtype=dtype)
+def _sub(addr, a, b):
+    return torch.sub(a, b)
+
+def _mul(addr, a, b):
+    return torch.mul(a, b)
+
+def _div(addr, a, b):
+    return torch.div(a, b)
+
+def _first(addr, x):
+    if isinstance(x, list):
+        return x[0]
+    elif torch.is_tensor(x):
+        if len(x.size()) > 1 and (not(x.shape[0] == 1 and x.shape[1] == 1)):
+            x = torch.squeeze(x)
+        return x[0]
+
+def _second(addr, x):
+    if isinstance(x, list):
+        return x[1]
+    elif torch.is_tensor(x):
+        if len(x.size()) > 1 and (not(x.shape[0] == 1 and x.shape[1] == 1)):
+            x = torch.squeeze(x)
+        return x[1]
+
+def _last(addr, x):
+    #import pdb; pdb.set_trace()
+    if isinstance(x, list):
+        return x[-1]
+    elif torch.is_tensor(x):
+        if len(x.size()) > 1 and (not(x.shape[0] == 1 and x.shape[1] == 1)):
+            x = torch.squeeze(x)
+        return x[-1]
+
+def _rest(addr, x):
+    if isinstance(x, list):
+        return x[1:]
+    elif torch.is_tensor(x):
+        if len(x.size()) > 1 and (not(x.shape[0] == 1 and x.shape[1] == 1)):
+            x = torch.squeeze(x)
+        return x[1:]
+
+def _lessthan(addr, a, b):
+    return torch.Tensor([a < b])
+
+def _greaterthan(addr, a, b):
+    return torch.Tensor([a > b])
+
+def _equals(addr, a, b):
+    return torch.Tensor([a == b])
+
+def _greaterthanequal(addr, a, b):
+    return torch.Tensor([a >= b])
+
+def _lessthanequal(addr, a, b):
+    return torch.Tensor([a <= b])
+
+def _and(addr, a, b):
+    return torch.Tensor([a and b])
+
+def _or(addr, a, b):
+    return torch.Tensor([a or b])
+
+def _log(addr, x):
+    return torch.log(x)
+
+def _normal(addr, mu, sigma):
+    return Normal(mu, sigma)
+
+def _beta(addr, a, b):
+    return dist.beta.Beta(concentration1=a, concentration0=b)
+
+def _gamma(addr, concentration, rate):
+    return dist.gamma.Gamma(concentration=concentration, rate=rate)
+
+def _uniform(addr, low, high):
+    return dist.uniform.Uniform(low=low, high=high)
+
+def _exponential(addr, rate):
+    return dist.exponential.Exponential(rate=rate)
+
+def _discrete(addr, probs):
+    return dist.categorical.Categorical(probs=probs)
+
+def _dirichlet(addr, concentration):
+    return dist.dirichlet.Dirichlet(concentration=concentration)
+
+def _beroulli(addr, probs):
+    return dist.bernoulli.Bernoulli(probs=probs)
+
+def _empty(addr, x):
+    return torch.Tensor([len(x) == 0])
+
+def _cons(addr, x1, x2):
+    if not torch.is_tensor(x1):
+        if isinstance(x1, list):
+            x1 = torch.tensor(x1, dtype=x.dtype)
         else:
-            x = torch.tensor([x], dtype=dtype)
-    return x
+            x1 = torch.tensor([x1], dtype=x.dtype)
+    if not torch.is_tensor(x2):
+        if isinstance(x2, list):
+            x2 = torch.tensor(x2, dtype=x.dtype)
+        else:
+            x2 = torch.tensor([x2], dtype=x.dtype)
+    try:
+        if len(x1.size()) > 1:
+            x1 = torch.squeeze(x1)
+        if len(x2.size()) > 1:
+            x2 = torch.squeeze(x2)
+        value = torch.cat((x1, x2))
+    except:
+        raise AssertionError('Cannot append the torch tensors')
 
-def _first(x):
-    if isinstance(x, list):
-        return x[0]
-    elif torch.is_tensor(x):
-        if len(x.size()) > 1:
-            x = torch.squeeze(x)
-        return x[0]
-
-def _second(x):
-    if isinstance(x, list):
-        return x[1]
-    elif torch.is_tensor(x):
-        if len(x.size()) > 1:
-            x = torch.squeeze(x)
-        return x[1]
-
-def _last(x):
-    if isinstance(x, list):
-        return x[-1]
-    elif torch.is_tensor(x):
-        if len(x.size()) > 1:
-            x = torch.squeeze(x)
-        return x[-1]
-
+    return value
 
 def push_addr(alpha, value):
     return alpha + value
 
-
 # Primitives
 env = {
-       'normal':       Normal,
        'push-address': push_addr,
-       '+':            torch.add,
-       '-':            torch.sub,
-       '*':            torch.mul,
-       '/':            torch.div,
-       'vector':       lambda *x: _vector(x),
-       'hash-map':     lambda *x: _hashmap(x),
-       'first':        lambda x: _first(x),
-       'second':       lambda x: _second(x),
-       'last':         lambda x: _last(x),
-       'rest':         lambda x: x[1:],
-       'get':          lambda x, idx: _get(x, idx),
-       'append':       lambda x, y: _append(x, y),
-       'remove':       lambda x, idx: _remove(x, idx),
-       'put':          lambda x, idx, value: _put(x, idx, value),
-       '<':            lambda a, b: a < b,
-       '>':            lambda a, b: a > b,
-       '=':            lambda a, b: a == b,
-       '>=':           lambda a, b: a >= b,
-       '<=':           lambda a, b: a <= b,
-       'or':           lambda a, b: a or b,
-       'and':          lambda a, b: a and b,
-       'sqrt':         lambda x: _squareroot(x)
+       'empty?':       _empty,
+       '+':            _add,
+       '-':            _sub,
+       '*':            _mul,
+       '/':            _div,
+       'vector':       _vector,
+       'hash-map':     _hashmap,
+       'first':        _first,
+       'second':       _second,
+       'last':         _last,
+       'peek':         _last,
+       'rest':         _rest,
+       'get':          _get,
+       'nth':          _get,
+       'append':       _append,
+       'remove':       _remove,
+       'cons':         _cons,
+       'conj':         _append,
+       'put':          _put,
+       '<':            _lessthan,
+       '>':            _greaterthan,
+       '=':            _equals,
+       '>=':           _greaterthanequal,
+       '<=':           _lessthanequal,
+       'or':           _or,
+       'and':          _and,
+       'log':          _log,
+       'sqrt':         _squareroot,
+       'normal':       _normal,
+       "beta":         _beta,
+       "gamma":        _gamma,
+       "uniform":      _uniform,
+       "exponential":  _exponential,
+       "discrete":     _discrete,
+       "dirichlet":    _dirichlet,
+       "bernoulli":    _beroulli,
+       "flip":         _beroulli,
+       "uniform-continuous": _uniform
       }
